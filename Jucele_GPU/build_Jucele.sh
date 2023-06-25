f060c5b8688c1b495c27deb237e94739fc45a5c0
Alex Fallin <waf13@txstate.edu>
2023-06-25 04:28:25 -0500

    jucele

--- Jucele_GPU/build_Jucele.sh
+++ Jucele_GPU/build_Jucele.sh
@@ -4,4 +4,14 @@ nvcc -O3 -arch=sm_70 JuceleMST.cu -o jucelemst
 
 echo 'Converting inputs to Jucele format'
 cd inputs/
+ln -s ../../Inputs inputs
 g++ -O3 -march=native ecl2jucele_mst_genweights.cpp -o gconv
+./gconv inputs/2d-2e20.sym.egr 2d-2e20.sym.egr.jmst
+./gconv inputs/delaunay_n24.egr delaunay_n24.egr.jmst
+./gconv inputs/r4-2e23.sym.egr r4-2e23.sym.egr.jmst
+./gconv inputs/citationCiteseer.egr citationCiteseer.egr.jmst
+./gconv inputs/europe_osm.egr europe_osm.egr.jmst
+./gconv inputs/USA-road-d.NY.egr USA-road-d.NY.egr.jmst
+./gconv inputs/coPapersDBLP.egr coPapersDBLP.egr.jmst
+./gconv inputs/internet.egr internet.egr.jmst
+./gconv inputs/USA-road-d.USA.egr inputs/USA-road-d.USA.egr.jmst
