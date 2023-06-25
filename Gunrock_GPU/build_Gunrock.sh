2a22ed1b9ad88f8d0fb613f130a28eedefc32332
Alex Fallin <waf13@txstate.edu>
2023-06-25 17:35:03 -0500

    gunrock build

--- Gunrock_GPU/build_Gunrock.sh
+++ Gunrock_GPU/build_Gunrock.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-echo 'ECL-MST Runs'
-echo 'Creating symlink to inputs'
+echo 'Building Gunrock'
+git clone https://github.com/alexfallin/gunrock_w_timing.git
+cd gunrock_w_timing
+mkdir build && cd build
+cmake .. -D CMAKE_CUDA_ARCHITECTURES=70
+make mst
+cp bin/mst ../../gunrock_mst
 
-ln -s ../Inputs inputs
 
-echo 'Running all files...'
+echo 'Converting inputs'
+cd ../../inputs/
+ln -s ../../Inputs inputs
 
-for FILE in inputs/*.egr
-do
-	./mst $FILE >> ecl_mst_out.csv
-done
+g++ -O3 -march=native ecl2mtx_genweights.cpp -o gconv
 
-echo 'copying results to root directory'
-
-cp ecl_mst_out.csv ..
+./gconv inputs/2d-2e20.sym.egr 2d-2e20.sym.egr.mtx
+./gconv inputs/amazon0601.egr amazon0601.egr.mtx
+./gconv inputs/as-skitter.egr as-skitter.egr.mtx
+./gconv inputs/citationCiteseer.egr citationCiteseer.egr.mtx
+./gconv inputs/cit-Patents.egr cit-Patents.egr.mtx
+./gconv inputs/coPapersDBLP.egr coPapersDBLP.egr.mtx
+./gconv inputs/delaunay_n24.egr delaunay_n24.egr.mtx
+./gconv inputs/europe_osm.egr europe_osm.egr.mtx
+./gconv inputs/in-2004.egr in-2004.egr.mtx
+./gconv inputs/internet.egr internet.egr.mtx
+./gconv inputs/kron_g500-logn21.egr kron_g500-logn21.egr.mtx
+./gconv inputs/r4-2e23.sym.egr r4-2e23.sym.egr.mtx
+./gconv inputs/rmat16.sym.egr rmat16.sym.egr.mtx
+./gconv inputs/rmat22.sym.egr rmat22.sym.egr.mtx
+./gconv inputs/soc-LiveJournal1.egr soc-LiveJournal1.egr.mtx
+./gconv inputs/USA-road-d.NY.egr USA-road-d.NY.egr.mtx
+./gconv inputs/USA-road-d.USA.egr USA-road-d.USA.egr.mtx
---
+++ Gunrock_GPU/inputs/ecl2mtx_genweights_cugraph.cpp
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "ECLgraph.h"
+
+int main(int argc, char* argv[]) {
+  printf("ECL to MTX Graph Converter (%s)\n", __FILE__);
+  printf("Copyright 2022 Texas State University\n");
+
+  if (argc != 3) {
+    fprintf(stderr, "USAGE: %s input_file_name output_file_name\n\n", argv[0]);
+    exit(-1);
+  }
+
+  ECLgraph g = readECLgraph(argv[1]);
+
+  printf("%s\t#name\n%d\t#nodes\n%d\t#edges\n", argv[1], g.nodes, g.edges);
+  if (g.eweight != NULL) printf("yes\t#weights\n"); else printf("no\t#weights\n");
+  // assign weights if needed
+  if (g.eweight == NULL) {
+    g.eweight = new int[g.edges];
+    for (int i = 0; i < g.nodes; i++) {
+      for (int j = g.nindex[i]; j < g.nindex[i + 1]; j++) {
+        const int k = g.nlist[j];
+        g.eweight[j] = 1 + ((i * k) % g.nodes);
+        if (g.eweight[j] < 0) g.eweight[j] = -g.eweight[j];
+      }
+    }
+    printf("added missing weights using ECL-MST method\n");
+  }
+
+  FILE* fout = fopen(argv[2], "wt");
+  if (fout == NULL) {
+    fprintf(stderr, "ERROR: could not open output file %s\n\n", argv[2]);
+    exit(-1);
+  }
+
+  fprintf(fout, "%%%%MatrixMarket matrix coordinate real symmetric\n");
+  fprintf(fout, "%d ", g.nodes);
+  fprintf(fout, "%d ", g.nodes);
+  fprintf(fout, "%d\n", g.edges / 2);
+  for (int src = 0; src < g.nodes; src++) {
+    for (int j = g.nindex[src]; j < g.nindex[src + 1]; j++) {
+      if (src > g.nlist[j]) {
+        fprintf(fout, "%d ", src + 1);
+        fprintf(fout, "%d ", g.nlist[j] + 1);
+        fprintf(fout, "%.1f\n", (float) g.eweight[j]);
+      }
+    }
+  }
+
+  fclose(fout);
+  freeECLgraph(g);
+  return 0;
+}
