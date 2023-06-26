b00aa0101a5f5253b7b216f3a4a0b0efa2c333c0
Alex Fallin <waf13@txstate.edu>
2023-06-25 23:34:23 -0500

    cuGraph attempts at build

--- cuGraph_GPU/build_cuGraph.sh
+++ cuGraph_GPU/build_cuGraph.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-echo 'ECL-MST Runs'
-echo 'Creating symlink to inputs'
+echo 'Building cuGraph'
 
-ln -s ../Inputs inputs
-
-echo 'Running all files...'
-
-for FILE in inputs/*.egr
-do
-	./mst $FILE >> ecl_mst_out.csv
-done
-
-echo 'copying results to root directory'
-
-cp ecl_mst_out.csv ..
+git clone https://github.com/rapidsai/cugraph.git
+cd cugraph/
+git checkout origin/branch-22.10
+conda env create --name cugraph_dev --file conda/environments/cugraph_dev_cuda11.0.yml
+conda env update --name cugraph_dev --file conda/environments/cugraph_dev_cuda11.0.yml
